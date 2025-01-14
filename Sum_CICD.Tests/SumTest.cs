e5ce10dfdb4f6df078a83fa86a93b1f6b1b3901d
Riccardo <riccardo.pausig@gmail.com>
2025-01-14 12:06:14 +0100

    Add more tests

--- Sum_CICD.Tests/SumTest.cs
+++ Sum_CICD.Tests/SumTest.cs
@@ -14,5 +14,17 @@ namespace Sum_CICD.Tests
         {
             Assert.Equal(5, _sum.Sum(2, 3));
         }
+
+        [Fact]
+        public void Should_Return_Sum_Of_Two_Negative_Integers()
+        {
+            Assert.Equal(-5, _sum.Sum(-2, -3));
+        }
+
+        [Fact]
+        public void Should_Return_Sum_Of_Two_Mixed_Integers()
+        {
+            Assert.Equal(1, _sum.Sum(-2, 3));
+        }
     }
 }
